c2077b0054b2c7b36c55ad3c6ff5541782d1bc14
Ahmad Saad <ahmadsd97@gmail.com>
2020-02-21 09:04:41 -0500

    Second Commit

--- app/src/main/java/temple/edu/mobisiblemouse/MainActivity.java
+++ app/src/main/java/temple/edu/mobisiblemouse/MainActivity.java
@@ -11,7 +11,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setTitle("Mobisible Mouse");
+        /*
+        * Steps to perform:
+        * 1) Get user permission for sound detection, bluetooth and camera usage
+        * 2) Either use OpenCV for motion detection, or use microphone, speaker
+        * and vibration detection for hand gesture and movement tracking.
+        * 3) Map the motion to a 2D plane
+        * 4) Send input to the computer over bluetooth
+        * 5) Make the computer perform  mouse actions based on this data. Will have
+        * to develop a simple software to connect with phone and perform the actions
+        * */
 
-        
     }
 }
