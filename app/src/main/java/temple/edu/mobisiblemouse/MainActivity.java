909b9aba8415abefd79c715584e99bbce4e3ff8b
Ahmad Saad <ahmadsd97@gmail.com>
2020-03-04 11:37:48 -0500

    Accessing Proximity Sensor to get yDisplacemnt values

--- app/src/main/java/temple/edu/mobisiblemouse/MainActivity.java
+++ app/src/main/java/temple/edu/mobisiblemouse/MainActivity.java
@@ -1,11 +1,20 @@
 package temple.edu.mobisiblemouse;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
+    private SensorManager sensorManager;
+    private Sensor sensor;
+    TextView txt;
+    float yDisplacement, prevValue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +30,39 @@ public class MainActivity extends AppCompatActivity {
         * 5) Make the computer perform  mouse actions based on this data. Will have
         * to develop a simple software to connect with phone and perform the actions
         * */
+        txt = findViewById(R.id.tv);
+        sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        if(sensorEvent.sensor.getType() == Sensor.TYPE_PROXIMITY){
+            float currentValue = sensorEvent.values[0];
+            //yDisplacement = prevValue - currentValue;
+            //Toast.makeText(this, currentValue+"",Toast.LENGTH_SHORT).show();
+            txt.setText(sensorEvent.values[0]+"");
+            //txt.setText("The finger moved: " + yDisplacement + " cm!");
+            prevValue = currentValue;
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
 
     }
 }
